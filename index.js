7b5ec16eee9fa010eda09914f20597ec86c65a0b
Samat <47773886+SamatFontendDev@users.noreply.github.com>
2019-09-30 17:57:19 +0300

    tz-2.1 done!

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-const path = require('path');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
+const intervalArg = process.argv[2];
+const timeOutArg = process.argv[3];
+
+const myFunc = (interval, timeOut) => {
+    app.get('*', function(req, res) {
+        const timeOutput = setInterval(() => {
+            let date = new Date(); 
+            let now_utc =  Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),
+            date.getUTCHours(), date.getUTCMinutes());
+            console.log(now_utc);
+        }, interval);
+        setTimeout(() => { 
+            clearInterval(timeOutput);
+            let date = new Date();
+            let year = date.getFullYear();
+            let month = date.getMonth() + 1;
+            let dateNum = date.getDate();
+            let hours = date.getHours()
+            let minutes = date.getMinutes();
+    
+            res.send(
+                `<div>
+                    <p>Дата: ${dateNum}.${month}.${year}</p>
+                    <p>Время: ${hours}.${minutes}</p>
+                </div>`
+            );
+        }, timeOut);
+    });
+} 
+
+myFunc(intervalArg, timeOutArg);
+
+app.listen(3000, function () {
+    console.log('Серевер запущен!');
+});
